d2d72fdc98b2800d68494b25de5dd1d4a4f731a6
anurag-6 <anuragr8129@gmail.com>
2024-10-15 12:28:27 +0530

    user input using pointers

--- main.go
+++ main.go
@@ -9,11 +9,33 @@ func main(){
 
     fmt.Printf("Welcome to the %v booking app...\n", conferenceName)
     fmt.Printf("We have total of %v tickets and %v are available\n", MaxTickets, remainingTickets)
-    fmt.Print("Get your tickets here to attend")
+    fmt.Print("Get your tickets here to attend\n")
 
-    var userName string = "Anurag"
-    var userTickets = 2
-    fmt.Printf("\nUser %v has booked %v ticket (s)\n", userName, userTickets)
+    var firstName string
+    var lastName string
+    var email string
+    var userTickets uint
+
+    // uses pointer conecpt to access the memory location of vaiable
+    fmt.Println("Enter the First Name: ")
+    fmt.Scan(&firstName)
+
+    fmt.Println("Enter the Last Name:")
+    fmt.Scan(&lastName)
+
+    fmt.Println("Enter the Email address:")
+    fmt.Scan(&email)
+
+    fmt.Println("Enter the number of tickets needed:")
+    fmt.Scan(&userTickets)
+
+    remainingTickets = remainingTickets - userTickets
+
+    fmt.Printf("Thankyou %v %v for booking %v tickets\nYou will recieve a confirmation email at %v shortly\n", firstName, lastName, userTickets, email)
+
+    fmt.Printf("Remaining tickets for %v conferece = %v", conferenceName, remainingTickets)
+
+
 
 
 }
